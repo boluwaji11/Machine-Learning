f3571a56a6278c5dc462ac848e6682aaf0fbd67e
Boluwaji Oyewumi <boluwajioyewumi@gmail.com>
2021-03-29 13:57:23 -0500

    digits visualized

---
+++ ml_1.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_digits
+
+digits = load_digits()
+
+# print(digits.DESCR)
+
+# print(digits.data[13])
+
+# print(digits.data.shape)
+
+# print(digits.target[13])
+# print(digits.target.shape)
+
+print(digits.images[13])
+
+import matplotlib.pyplot as plt
+
+figure, axes = plt.subplots(nrows=4, ncols=6, figsize=(6, 4))
+
+for item in zip(axes.ravel(), digits.images, digits.target):
+    axes, image, target = item
+    axes.imshow(image, cmap=plt.cm.gray_r)
+    axes.set_xticks([])
+    axes.set_yticks([])
+    axes.set_title(target)
+plt.tight_layout()
+plt.show()
